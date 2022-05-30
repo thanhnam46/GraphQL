58e7d01b23d484ec526fb2727586347c0b674a72
nam <thanhnam97@gmail.com>
2022-05-30 16:46:32 +0700

    #9 (48:20)- The Resolve Function #10 (53:16)- Testing Queries in Graphiql #11 (59:49)- GraphQL ID Type #12 (1:03:36)- Author Type #13 (1:09:30)- Type Relations #14 (1:17:41)- GraphQL Lists #15 (1:25:56)- More on Root Queries #16 (1:29:39)- Connecting to mLab

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 
 const app = express();
-app.use('/graphql', graphqlHTTP({ schema }));
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 app.listen(4000, () => {
     console.log('Now listening for requests on port 4000');
 });
