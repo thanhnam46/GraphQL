2162c24b787d592480e33eb93b6205aabab7f681
nam <thanhnam97@gmail.com>
2022-05-31 12:15:59 +0700

    #19 (1:53:06)- More on Mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,9 @@ const {
     GraphQLList,
 } = graphql;
 
+const Book = require('../models/book');
+const Author = require('../models/author');
+
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
@@ -73,6 +76,43 @@ const RootQuery = new GraphQLObjectType({
     },
 });
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt },
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age,
+                });
+                return author.save();
+            },
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: { type: GraphQLString },
+                genre: { type: GraphQLString },
+                authorId: { type: GraphQLID },
+            },
+            resolve(parent, args) {
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId,
+                });
+                return book.save();
+            },
+        },
+    },
+});
+
 module.exports = new GraphQLSchema({
     query: RootQuery,
+    mutation: Mutation,
 });
