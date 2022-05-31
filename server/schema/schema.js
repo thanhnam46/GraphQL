9b63c73a98ec9ce473ceb53c2d850a80f859c9c0
nam <thanhnam97@gmail.com>
2022-05-31 14:53:52 +0700

    #21 (2:06:45)- GraphQL NonNull

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,6 +7,7 @@ const {
     GraphQLID,
     GraphQLInt,
     GraphQLList,
+    GraphQLNonNull,
 } = graphql;
 
 const Book = require('../models/book');
@@ -88,8 +89,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name: { type: GraphQLString },
-                age: { type: GraphQLInt },
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) },
             },
             resolve(parent, args) {
                 let author = new Author({
@@ -102,8 +103,8 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: { type: GraphQLString },
-                genre: { type: GraphQLString },
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                genre: { type: new GraphQLNonNull(GraphQLString) },
                 authorId: { type: GraphQLID },
             },
             resolve(parent, args) {
